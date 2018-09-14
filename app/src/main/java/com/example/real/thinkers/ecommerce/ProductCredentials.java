ef64eaa43353fe2f9b7a282dedbf2537d4428290
abbas <abbasm559@gmail.com>
2018-09-15 02:01:28 +0600

    refactor

--- app/src/main/java/com/example/real/thinkers/ecommerce/ProductCredentials.java
+++ app/src/main/java/com/example/real/thinkers/ecommerce/ProductCredentials.java
@@ -1,12 +1,12 @@
 package com.example.real.thinkers.ecommerce;
 
-public class ProductCriterias {
+public class ProductCredentials {
 
     private static final String titleKey = "titleKey";
     private static final String descriptionKye = "desctiptionKey";
     private static final String priceKey = "priceKey";
     private static final String imageKye = "imageKey";
 
-    public ProductCriterias() {
+    public ProductCredentials() {
     }
 }
