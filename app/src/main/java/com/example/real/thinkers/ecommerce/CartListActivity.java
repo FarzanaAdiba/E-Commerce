7b87dad839321a9723bd5f88e37153ce56f9cef4
Your Name <you@example.com>
2018-09-18 20:37:21 +0600

    Commit changes

--- app/src/main/java/com/example/real/thinkers/ecommerce/CartListActivity.java
+++ app/src/main/java/com/example/real/thinkers/ecommerce/CartListActivity.java
@@ -1,8 +1,12 @@
 package com.example.real.thinkers.ecommerce;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -37,6 +41,33 @@ public class CartListActivity extends AppCompatActivity {
         }
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
         cartLV.setAdapter(adapter);
+
+        cartLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(CartListActivity.this);
+                builder.setMessage("Want to buy?");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(CartListActivity.this,"Login here",Toast.LENGTH_LONG).show();
+                        //loginActivity will be added here
+                    }
+                });
+                builder.setNegativeButton("Not now", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+
     }
 
    /* private void loadData() {
